00b21e610bad8a5ee3f66d57997fb88af8d1dd58
tannabr23596 <tabrah13@my.centennialcollege.ca>
2023-04-17 22:08:31 -0400

    booking test

--- src/components/BookTest.jsx
+++ src/components/BookTest.jsx
@@ -1,14 +1,29 @@
 import React ,{ useState, useEffect } from 'react'
 import TimePicker from 'react-bootstrap-time-picker';
 import axios from 'axios';
+import {useNavigate} from 'react-router-dom'
+
 const BookTest = () => {
+  const navigate =   useNavigate()
 
   const apiUrl = "http://localhost:8087/certification";
   const centersUrl = "http://localhost:8087/centre"
   const [data, setData] = useState([]);
   const [center, setCenters] = useState([]);
+  const [state, setState] = useState({
+    
+        centreName:'',
+        address:'',
+        testdate:'',
+        time:'',
+        certificationName:'',
+        candidateId:'',
+    })
+
   const [showLoading, setShowLoading] = useState(true);
-  const [selectedCenter, setSelectedCenter] = useState('');
+  const bookTestStyle = {
+    padding: "1rem !important"
+  };
 
 
   useEffect(() => {
@@ -45,61 +60,65 @@ useEffect(() => {
   fetchCenters();
 }, []);
   const [time, setTime] = useState(0);
-
-  const handleTimeChange = (value) => {
-      setTime(value);
-  };
+  const handleInputChange=(e)=>{
+       
+    setState({
+        ...state,
+        [e.target.name]:e.target.value,
+        candidateId:localStorage.getItem('candidateId')
+    })
+}
+  
   const handleFormSubmit = (e) => {
     e.preventDefault()
-
-  const selectedCenterData = center.find(
-    (center) => center.centerCode === selectedCenter
-  );
-
-  // Update the address field with the selected test center's address
-  const addressField = document.getElementById('exampleFormControlInputCentreaName');
-  addressField.value = selectedCenterData.address;
+        axios.post('http://localhost:8087/test',state)
+        .then((response)=>{
+            console.log(response.data)
+            navigate('/view')
+        })
+        .catch((err)=>{
+            console.log(err)
+            navigate('/view')
+        })
+ 
   }
   return (
-    <div className=" p-5">
+    <div style={bookTestStyle}>
       <div className="col-md-6 mt-2 mx-auto bg bg-light p-5 border border-danger  border-width-5 text-dark">
         <h3>Book Your Test</h3>
         <form onSubmit={handleFormSubmit}>
-        <div className='form-group'>
-            <label htmlFor='testCenterName'>Test Centre Name</label>
-            <select className='form-control' id='testCenterName'  value={selectedCenter}
-  onChange={(e) => setSelectedCenter(e.target.value)}>
-              <option>Select</option>
-              {center.map((center) => (
-                <option key={center.centerCode} value={center.centerCode} >
-                  {center.centerName}
-                </option>
+        
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Test Centre Name</label>
+            <select class="form-control" id="exampleFormControlSelect1" name="centreName" onChange={handleInputChange} value={state.centreName}>
+              <option>Select centre name</option>
+              {data.map((centre, index) => (
+                <option key={index}>{centre.centerName}</option>
               ))}
             </select>
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Address</label>
-            <input type="text" class="form-control" id="exampleFormControlInputCentreaName" placeholder="Address" />
+            <input type="text" class="form-control" id="exampleFormControlInputCentreaName" placeholder="Address" name="address" onChange={handleInputChange} value={state.address} />
           </div>
 
           <div class="form-group">
             <label for="exampleFormControlInput1">Test Date</label>
-            <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="Test Date" />
+            <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="Test Date" name="testdate" onChange={handleInputChange} value={state.testdate}/>
           </div>
 
           <div class="form-group">
             <label for="exampleFormControlInput1">Time</label>
-            <TimePicker
+            <input type="text"
                         id="myTimePicker"
-                        name="myTimePicker"
-                        value={time}
-                        onChange={handleTimeChange}
+                        name="time"
+                        onChange={handleInputChange} value={state.time}
                     />
           </div>
 
           <div class="form-group">
             <label for="exampleFormControlInput1">Certification</label>
-            <select class="form-control" id="exampleFormControlSelect1">
+            <select class="form-control" id="exampleFormControlSelect1" name="certification" onChange={handleInputChange} value={state.certificationName}>
               <option>Select certification</option>
               {data.map((cert, index) => (
                 <option key={index}>{cert.certificationName}</option>
